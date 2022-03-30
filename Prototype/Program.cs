0c8d69be624b7290ead85ede55852a82cbd910e9
Andre Mohren <andre.mohren@tetys.de>
2022-03-30 14:01:56 +0200

    OSX bugfix.

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Prototype;
 
 // TODO this is a hack around a stride bug!
+// TODO If the runtime db files are not deleted, the whole app will only render on the first start!
+
+// TODO under windows-x64 the data dir is used, so we clear all generated stuff.
 File.WriteAllText("data/db/index", "");
 
 foreach (var directory in Directory.GetDirectories("data/db").Select(e => e.Replace("\\", "/")))
@@ -8,6 +11,9 @@ foreach (var directory in Directory.GetDirectories("data/db").Select(e => e.Repl
 	if (directory is not ("data/db/tmp" or "data/db/bundles"))
 		Directory.Delete(directory, true);
 }
+
+// TODO under osx-arm64 however the runtime generated db files are in another location
+Directory.Delete("local", true);
 
 using var prototype = new PrototypeGame();
 prototype.Run();
